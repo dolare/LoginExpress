ceebdc8ce5fe492cf73fd5b44590ba86941d1442
dolare <dolarebrave@gmail.com>
2016-03-22 17:05:21 -0400

    watch

--- public/app.js
+++ public/app.js
@@ -1,80 +1,46 @@
-(function(){
-    'user strict';
+(function() {
+    'use strict';
 
-
-    var app = angular.module('app',[]);
-
-    app.factory('login',function($http){
-
-        var user = undefined;
-        var loginInfo = {};
-        var changes = [];
-        var change = function(data){
-            changes.forEach(function(fn){
-                fn(data);
-            });
-            loginInfo.info = data;
-            return loginInfo;
-        }
-        return {
-            login:function(user){
-                return $http.post('/api/login',user).then(change);
-            },
-
-            logout:function(){
-
-                return $http.delete('/api/login').then(change);
-
-            },
-
-            isLogin:function(){
-                return $http.get('/api/login').then(change);
-         
-            },
-
-            loginInfo:loginInfo,
-            onchange:function(fn){
-                changes.push(fn);
-            }
-        };
-    
-    });
-
-    app.service('login1',function($http){
-        this.login = function(){};
-        this.logout = function(){};
-        this.isLogin = function(){};
-    });
-
-
-    app.controller('mainCtrl',function($scope,login){
+    angular.module('app', [])
+        .factory('login', function($http, $rootScope) {
+            var loginInfo;
+            var change = function(data) {
+                loginInfo = data.data;
+            };
+            return {
+                login: function(user) {
+                    return $http.post('/api/login', user).then(change);
+                },
+                logout: function() {
+                    return $http.delete('/api/login').then(change);
+                },
+                isLogin: function() {
+                    return $http.get('/api/login').then(change);
+                },
+                getInfo: function(){
+                    return loginInfo;
+                }
+            };
+        })
+        .controller('MainCtrl', function($scope, login) {
+            $scope.login = login;
             login.isLogin();
-            $scope.logout = function(){
+            $scope.logout = function() {
                 login.logout();
             };
-                
-            login.onchange(function(data){
-                $scope.loginInfo = data.data;
-            });   
-            
-            
-
-    });
-
-
-    app.controller('loginCtrl',['$scope','login',
-        function($scope,login){
+            $scope.$watch('login.getInfo()', function(n, o) {
+                $scope.loginInfo = n;
+            });
+        })
+        .controller('LoginCtrl', function($scope, login) {
+            $scope.login = login;
             var self = this;
-            login.isLogin();
-            
             //self.login = login.login.bind(login);
-            self.login = function(user){
+            self.login = function(user) {
                 login.login(user);
             };
-
-            login.onchange(function(data){
-                self.loginInfo = data.data;
-            });            
-        }]);
-
+            $scope.$watch('login.getInfo()', function(n, o) {
+                self.loginInfo = n;
+            });
+        });
 })();
