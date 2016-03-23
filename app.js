163adeb9f4edcf69664ee435492d280dfe9addae
dolare <dolarebrave@gmail.com>
2016-03-23 12:15:12 -0400

    isolateScope

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
-//var RedisStore = require('connect-redis')(session);
-var MongoStore = require('connect-mongo')(session);
+var mongoStore = require('connect-mongo')(session);
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -15,7 +14,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -25,17 +24,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
-    secret:'keyboard cat',
-    resave:false,
-    saveUninitialized:true,
-   // store:new RedisStore()
-    store:new MongoStore({url:'mongodb://localhost:27017/test'})
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    store:new mongoStore({url:'mongodb://localhost/test'})
 }));
-
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/api/login',require('./routes/login'));
+app.use('/api/login', require('./routes/login'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- public/app.js
+++ public/app.js
@@ -1,52 +1,43 @@
-(function(){
-    'user strict';
+(function() {
+    'use strict';
 
-
-    var app = angular.module('app',[]);
-    app.controller('mainCtrl',function($scope,$http,$rootScope){
-            $scope.logout = function(){
-                $http.delete('/api/login').success(function(){
-                    $scope.user = undefined;
-                    $scope.nu = null;
-                    $rootScope.$broadcast('logout');
+    angular.module('app', [])
+        .controller('LoginCtrl', ['$scope', '$http',
+            function(scope, $http) {
+                
+                $http.get('/api/login').success(function(resp){
+                        scope.loginInfo = resp;
                 });
-            };
-            
-            $scope.$on('login sucess',function(e,user){
-                $scope.user = user;
-            });
+                
+                scope.logout = function() {
+                    $http.delete('/api/login').success(function(){
+                        scope.loginInfo = {};
+                    })
+                };
 
-    });
+                scope.onLogin = function(info){
+                    scope.loginInfo = info;
 
-
-    app.controller('loginCtrl',['$scope','$http','$rootScope',
-        function($scope,$http,$rootScope){
-            var self = this;
-            
-            $scope.$on('logout',function(){
-                self.user = undefined;   
-            })
-
-            $http.get('/api/login').success(function(
-            resp){
-                if(resp.username)
-                self.user = resp;
-                $rootScope.$broadcast('login sucess',self.user);
-
-            });
-
-            self.login = function(user){
-                $http.post('/api/login',user)
-                .then(function(data){
-                    if(data.data.username){
-                        self.user = data.data;
-                        $rootScope.$broadcast('login sucess',
-                            self.user);
-                    }
-                    else self.msg = data.data.msg;
-                });
-
+                }
+                
             }
-        }]);
-
+        ])
+        
+        .directive('loginForm',function($http){
+            return{
+                templateUrl:'login.html',
+                scope:{
+                    info:"=info2",
+                    login2:"&"
+                },
+                link:function(scope){
+                    scope.login = function(user) {
+                        $http.post('/api/login', user).then(function(data) {
+                            //scope.info = data.data;
+                            scope.login2({info:data.data});
+                        });
+                    }
+                }
+            }
+        });
 })();
